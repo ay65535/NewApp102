65c1939babc2b3336dc1d8d8f277981e78d1d0fc
ay65535 <ay65535@icloud.com>
2015-01-02 19:30:08 +0900

    Reformat codes

--- app/src/main/java/com/github/ay65535/newapp102/MainActivity.java
+++ app/src/main/java/com/github/ay65535/newapp102/MainActivity.java
@@ -1,7 +1,7 @@
 package com.github.ay65535.newapp102;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
